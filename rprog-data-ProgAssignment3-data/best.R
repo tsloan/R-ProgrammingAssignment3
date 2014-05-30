2e3224180c4cf0839e3051af5fd5a1eb638abe40
tsloan <terry@sloandrums.com>
2014-05-30 01:01:18 +0100

    Added code for extracting mortality at hospital

--- rprog-data-ProgAssignment3-data/best.R
+++ rprog-data-ProgAssignment3-data/best.R
@@ -1,4 +1,4 @@
-best <- function(state, outcome, y) {
+best <- function(state, outcome) {
   ## Check that state and outcome are valid
   ## check they are not missing
   if (missing(outcome)) stop("invalid outcome")
@@ -6,6 +6,16 @@ best <- function(state, outcome, y) {
 
   # above still does not catch an undefined object  
   
+  #######################################################################
+  ##  Relevant fields in outcome-of-care-measures.csv
+  ##  field index field name
+  ##   [2]     "Hospital.Name"
+  ##   [7]     "State"
+  ##   [15] "Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+  ##   [21] "Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+  ##   [27] "Number.of.Patients...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  
+  ######################################################################## 
   # Check the request outcome is valid
   validOutcomes <-c("heart attack", "heart failure", "pneumonia")
   goodOutcomes <- (validOutcomes == outcome)
@@ -15,10 +25,16 @@ best <- function(state, outcome, y) {
   # in goodOutcomes will be 1.
   if (sum(goodOutcomes) == 0) stop('invalid outcome')    
   
+  if (outcome == validOutcomes[1]) MortalityField <- 15
+  else if (outcome == validOutcomes[2]) MortalityField <- 21
+  else if (outcome == validOutcomes[3]) MortalityField <- 27
+  else stop('invalid outcome')
+  
+  #########################################################################
   ## Read outcome data
   data<-read.csv("outcome-of-care-measures.csv", colClasses="character")
-      
-    
+  
+  #########################################################################
   # Check the state is valid
   # The two letter state code is held in field 7 of 
   # outcome-of-care-measures.csv
@@ -33,11 +49,17 @@ best <- function(state, outcome, y) {
   goodStates <- (state == stateCodes)
   if (sum(goodStates) == 0) stop('invalid state')    
     
+  #######################################################################
   ## find the name of the hospital that has the best (i.e. lowest) 
   ## 30-day mortality for the specified outcome in that state.
   ##
+  
   ## get the hospitals in state
-  data[USstate == state]
+  statehospitals <- data[data[USstate]==state,]
+  # extract the hospital name and the mortality rates for the 
+  # desired outcome
+  statehospitals[,c(2,MortalityField)] 
+  #  sort these into ascending order by Mortality field, ignoring NAs
   
   
   ## Return hospital name in that state with lowest 30-day death
