dd6d6ac32053e2fcf34f852a883de78d53f2bc15
tsloan <terry@sloandrums.com>
2014-06-01 21:29:36 +0100

    added for loop for building rank of all hospitals in a state

--- rprog-data-ProgAssignment3-data/rankhospital.R
+++ rprog-data-ProgAssignment3-data/rankhospital.R
@@ -26,6 +26,16 @@ rankhospital <- function(state, outcome, num = "best") {
     data<-read.csv("outcome-of-care-measures.csv", 
                    colClasses="character", 
                    na.strings=c("Not Available","NA"))
+    #######################################################################
+    ##  Relevant fields in outcome-of-care-measures.csv
+    ##  field index field name
+    ##   [2]     "Hospital.Name"
+    ##   [7]     "State"
+    ##   [11] "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+    ##   [17] "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+    ##   [23] "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" 
+    
+    ######################################################################## 
     
     #########################################################################
     ## Check the state is valid 
@@ -49,10 +59,13 @@ rankhospital <- function(state, outcome, num = "best") {
     
     validOutcomes <-c("heart attack", "heart failure", "pneumonia")
     
-    if (outcome == validOutcomes[1]) OutcomeField <- 11
-    else if (outcome == validOutcomes[2]) OutcomeField <- 17
-    else if (outcome == validOutcomes[3]) OutcomeField <- 23
-    else stop('invalid outcome')
+    if (outcome == validOutcomes[1]) { 
+        OutcomeField <- 11
+    } else if (outcome == validOutcomes[2]) {
+        OutcomeField <- 17
+    } else if (outcome == validOutcomes[3]){
+        OutcomeField <- 23
+    } else stop('invalid outcome')
     
     #########################################################################
     ## Check that num is valid 
@@ -87,7 +100,38 @@ rankhospital <- function(state, outcome, num = "best") {
     # in ascending order
     #########################################################################
     
-    order(statehospitals[,OutcomeField])
+    orderIndex<-order(statehospitals[,OutcomeField])
+    
+    #########################################################################
+    # Create a ranking table consisting of 
+    # hospital name, the value of the Outcome and the hospital rank
+    #########################################################################
+    
+    rank <- vector('character')
+    for (i in 1:length(orderIndex)){
+        line <- c( statehospitals[orderIndex[i],2],
+                   statehospitals[orderIndex[i],OutcomeField], i)
+        # get all the subsequent compare the value of the Outcome field with the next one
+        # if it equals it then sort by hospital name
+        j <- i + 1
+        while (j < length(orderIndex) && ){
+            
+        }
+        rank <- c(rank, line)             
+    }
+    rankmatrix<-matrix(rank,3,nrow(statehospitals))
+    
+    if (num == 1 || num == "best"){
+        # extract the names of all the hospitals with the 
+        # best rank
+        if 
+        #extract the name of the best hospital
+        statehospitals[orderIndex[1],c(2)]
+        # sort the hospitals by name 
+        order(rank)
+    }
+    #check if num == nrow since if it does then need to be careful
+    # about handling ties
     
     
     
